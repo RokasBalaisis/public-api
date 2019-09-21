01354ded377a0020ea4b01dd24c1d5f1e820d65a
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-21 17:40:04 +0300

    jwt rework

--- app/User.php
+++ app/User.php
@@ -2,58 +2,38 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+    use Illuminate\Notifications\Notifiable;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-use Tymon\JWTAuth\Contracts\JWTSubject;
-
-class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
-{
-    use Authenticatable, Authorizable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
-
-    
-    // Rest omitted for brevity
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
+    class User extends Authenticatable implements JWTSubject
     {
-        return $this->getKey();
-    }
+        use Notifiable;
 
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'name', 'email', 'password',
+        ];
 
-}
+        /**
+         * The attributes that should be hidden for arrays.
+         *
+         * @var array
+         */
+        protected $hidden = [
+            'password',
+        ];
+
+        public function getJWTIdentifier()
+        {
+            return $this->getKey();
+        }
+        public function getJWTCustomClaims()
+        {
+            return [];
+        }
+    }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,8 @@ $app = new Laravel\Lumen\Application(
 
 // SERVICE ALIASES
 class_alias(Pbmedia\LaravelFFMpeg\FFMpegFacade::class, 'FFMpeg');
-// class_alias(Tymon\JWTAuth\Facades\JWTFactory::class, 'JWTFactory');
+class_alias(Tymon\JWTAuth\Facades\JWTAuth::class, 'JWTAuth');
+class_alias(Tymon\JWTAuth\Facades\JWTFactory::class, 'JWTFactory');
 
 $app->withFacades();
 
@@ -67,7 +68,7 @@ $app->middleware([
 ]);
 
 $app->routeMiddleware([
-    'auth' => App\Http\Middleware\Authenticate::class
+    'jwt.verify' => App\Http\Middleware\Authenticate::class
 ]);
 
 /*
