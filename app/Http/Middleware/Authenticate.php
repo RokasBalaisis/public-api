4ca85a5fb91b1722777b666ea34b97dae04244b8
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-23 22:58:08 +0300

    carbon include

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Auth\Factory as Auth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Carbon\Carbon;
 
 class Authenticate
 {
