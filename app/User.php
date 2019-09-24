a5a133f9fc489753814f9d6b9a77b69691b676a1
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 21:13:01 +0300

    api update

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 
     public function role()
     {
-        return $this->hasOne('App\Role', 'user_role');
+        return $this->belongsToMany('App\Role', 'user_role');
     }
 
 
