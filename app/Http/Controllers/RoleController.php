44a83325cb73c9ab0b64a3dbdcdbeb9c0e5e4888
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 20:24:50 +0300

    api update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -17,7 +17,7 @@ class RoleController extends Controller
      */
     public function index()
     {
-        $roles = Role::all();
+        $roles = Role::with('users')->get();
         return response()->json(['roles' => $roles], 200);
     }
 
