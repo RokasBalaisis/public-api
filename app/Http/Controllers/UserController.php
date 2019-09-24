da77d5f2ea12ac89be39b6efc5585cd7c215f4e1
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 20:15:57 +0300

    api debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::with('role')->get();
+        $users = User::all()->first()->role();
         return response()->json(['users' => $users], 200);
     }
 
