0d292c44a9479e5a65061f248e38dfc1d1b022ff
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 21:45:31 +0300

    api update

--- database/seeds/UserRolesTableSeeder.php
+++ database/seeds/UserRolesTableSeeder.php
@@ -15,11 +15,15 @@ class UserRoleTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $adminIsSet = false;
         $users = DB::table('users')->get();
         $rolesIds = DB::table('roles')->pluck('id')->toArray();
         foreach($users as $user)
         {
-            DB::table('user_role')->insert(['user_id' => $user->id, 'role_id' => $faker->randomElement($rolesIds)]);
+            if($adminIsSet == false)
+                DB::table('user_role')->insert(['user_id' => $user->id, 'role_id' => $rolesIds[0]]);
+            else
+                DB::table('user_role')->insert(['user_id' => $user->id, 'role_id' => $faker->randomElement($rolesIds)]);
         }
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 
 class UsersTableSeeder extends Seeder
@@ -13,6 +14,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        // create initial admin account
+        DB::table('users')->insert(['name' => "Admin", 'email' => 'admin@admin.lt', 'password' => Hash::make('admin')]);
         // create 10 users using the user factory
         factory(App\User::class, 10)->create();
     }
