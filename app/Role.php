c4ad85ae0b54d00ff2a7cf2dff32ab79bb34da4c
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-24 20:58:19 +0300

    api update

--- app/Role.php
+++ app/Role.php
@@ -21,7 +21,7 @@ class Role extends Model
 
     public function users()
     {
-        return $this->belongsToMany('App\User', 'user_roles');
+        return $this->belongsToMany('App\User', 'user_role');
     }
 
 }
