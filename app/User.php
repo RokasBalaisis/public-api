42c14c80d19a9866e40d90c4c38c6a95abfd0faa
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-25 18:43:01 +0300

    hide jwt values on json from User

--- app/User.php
+++ app/User.php
@@ -32,7 +32,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password', 'pivot',
+        'password', 'pivot', 'jwt', 'exp'
     ];
 
 
