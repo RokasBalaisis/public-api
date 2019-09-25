430455fa310de4a2fbca15bf0198f153af1a56e2
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-25 18:55:57 +0300

    hide user jti value

--- app/User.php
+++ app/User.php
@@ -32,7 +32,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password', 'pivot', 'jwt', 'exp'
+        'password', 'pivot', 'jti', 'exp'
     ];
 
 
