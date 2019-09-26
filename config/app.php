4c803092aa75c72bd331fc784da6afbce57c7a85
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-27 00:12:54 +0300

    leeway fix

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE', 'Europe/Vilnius'),
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
