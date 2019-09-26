9018925c0b49fa7bec4645892a0113b3ed12ddc7
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-27 00:00:13 +0300

    api timezone fix

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
-    'timezone' => env('APP_TIMEZONE'),
+    'timezone' => env('APP_TIMEZONE', 'UTC'),
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
