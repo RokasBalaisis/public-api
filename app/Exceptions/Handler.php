f465e6753a8ead5c25aa7335bb30b5587cf9d5c4
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-30 14:09:16 +0300

    api update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,8 +48,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        dd($exception);
         if ($exception instanceof ModelNotFoundException) {
-            return response()->json(['message' => 'Not Found!'], 404);
+            return response()->toJson(['message' => 'Not Found!'], 404);
         }
         return parent::render($request, $exception);
     }
