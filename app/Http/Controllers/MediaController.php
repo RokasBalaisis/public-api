7a53df526e76140681a5fd735cf1aba9cf6cbd04
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-09-30 03:09:23 +0300

    media with file upload method

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use App\User;
 use App\Role;
 use App\Media;
+use Carbon\Carbon;
 
 class MediaController extends Controller
 {
@@ -34,6 +36,9 @@ class MediaController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => ['required', 'min:3', 'regex:/^[A-Za-z]+$/'],
+            'short_description' => ['required'],
+            'description' => ['required'],
+            'trailer_url' => ['regex:www.youtube(?:-nocookie)?.com\/(?:v|embed)\/([a-zA-Z0-9-_]+).*'],
             'image' => ['required', 'array', 'min:3', 'max:3'],
             'image.*' => ['required','file','mimes:jpg,jpeg,png,bmp'],
         ]);
@@ -42,13 +47,24 @@ class MediaController extends Controller
             return response()->json([ 'message'=> $validator->errors()->first() ], 422);
         }
 
+        $media = new Media;
+        $media->name = $request->name;
+        $media->short_description = $request->short_description;
+        $media->description = $request->description;
+        $media->trailer_url = $request->trailer_url;
+        $media->save();
+
+        $file_data = array();
+
         $counter = 0;
         foreach($request->image as $image)
         {
-            $image->storeAs('images', 'image['.$counter.'].'.$image->getClientOriginalExtension());
+            array_push($file_data, array(['media_id' => $media->id, 'folder' => 'images', 'name' => 'image['.$counter.'].'.$image->getClientOriginalExtension(), 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]));
+            $image->storeAs('media/'.$media->id.'/images', 'image['.$counter.'].'.$image->getClientOriginalExtension());
             $counter++;
         }
-        return response()->json(['message' => 'Files uploaded'], 200);
+        DB::table('media_files')->insert($file_data);
+        return response()->json(['message' => 'Media has been successfully created', 'media' => $media], 200);
     }
 
     /**
