a716d8677f5beca9cc7be818252d06a07d9a2189
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-10-07 12:02:41 +0300

    api debug

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -161,7 +161,7 @@ class MediaController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-       try {
+      // try {
             $media = Media::with('files')->find($id);
             if($request->name != null)
                 $media->name = $request->name;
@@ -225,10 +225,10 @@ class MediaController extends Controller
             //return successful response
             return response()->json(['message' => 'Media information has been successfuly updated', 'media' => $media], 200);
 
-        } catch (\Exception $e) {
+        //} catch (\Exception $e) {
             //return error message
-           return response()->json(['message' => 'Media edit failed!'], 409);
-       }
+       //    return response()->json(['message' => 'Media edit failed!'], 409);
+      // }
     }
 
     /**
