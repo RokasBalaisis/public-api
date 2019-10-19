7204c39622dd8f7ab033b2070abac1bb7ae8070e
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-10-19 14:42:48 +0300

    api update

--- database/seeds/MediaTableSeeder.php
+++ database/seeds/MediaTableSeeder.php
@@ -7,6 +7,7 @@ use App\Role;
 use App\User;
 use App\Media;
 use App\MediaType;
+use Carbon\Carbon;
 
 class MediaTableSeeder extends Seeder
 {
@@ -17,13 +18,15 @@ class MediaTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $current_timestamp = Carbon::now();
         $media_types = MediaType::all();
         $data = array();
         $amount = 100;
         for($i=0; $i < $amount; $i++)
         {
             foreach($media_types as $media_type)
-                array_push($data, (['media_type_id' => $media_type->id ,'name' => $faker->unique()->company, 'short_description' => $faker->unique()->paragraph(5, true), 'description' => $faker->unique()->text(200), 'trailer_url' => 'www.youtube.com/embed/meMc5MWvBLo']));
+                array_push($data, (['media_type_id' => $media_type->id ,'name' => $faker->unique()->company, 'short_description' => $faker->unique()->paragraph(5, true), 'description' => $faker->unique()->text(200), 'trailer_url' => 'www.youtube.com/embed/meMc5MWvBLo', 'created_at' => $current_timestamp, 'updated_at' => $current_timestamp]));
         }
+        DB::table('media')->insert($data);
     }
 }
