6aac11e81ebd674e911dd7ab52ea1ba7a5f81604
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-10-20 23:10:28 +0300

    api update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
+use Faker\Generator as Faker;
 
 
 class UsersTableSeeder extends Seeder
@@ -13,11 +14,17 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         // create initial admin account
-        DB::table('users')->insert(['username' => "Admin", 'email' => 'admin@admin.lt', 'password' => Hash::make('admin'), 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]);
-        // create 10 users using the user factory
-        factory(App\User::class, 100)->create();
+        $data = array();
+        array_push($data, (['username' => "Admin", 'email' => 'admin@admin.lt', 'password' => Hash::make('admin'), 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]));
+        $amount = 100;
+        for($i=0; $i < $amount; $i++)
+        {
+            $current_timestamp = Carbon::now();
+            array_push($data, (['username' => $faker->unique()->userName, 'email' => $faker->unique()->email, 'password' => Hash::make('12345'), 'created_at' => $current_timestamp, 'updated_at' => $current_timestamp]));
+        }
+        DB::table('users')->insert($data);
     }
 }
