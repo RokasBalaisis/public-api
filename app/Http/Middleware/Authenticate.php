a93cce48d41eba7c77aafbbd819cf0ceac5ffaac
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-10-29 19:24:44 +0200

    api update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -43,8 +43,8 @@ class Authenticate
     {
         if($this->auth->guard($guard)->guest())
             return response()->json(['message' => 'Unauthorized'], 401);
-        $payload = \JWTAuth::manager()->getJWTProvider()->decode(\JWTAuth::getToken()->get());
-        $currentToken = \JWTAuth::getToken()->get();
+        $payload = JWTAuth::manager()->getJWTProvider()->decode(JWTAuth::getToken()->get());
+        $currentToken = JWTAuth::getToken()->get();
             
             $currentuser = User::find($payload['sub']);
             if($payload['exp'] < Carbon::now()->timestamp)
@@ -62,8 +62,8 @@ class Authenticate
                 if (! $new_token = $this->auth->guard($guard)->fromUser($currentuser)) {
                     return response()->json(['message' => 'Unauthorized'], 401);
                 }
-                \JWTAuth::invalidate($currentToken);
-                $new_payload = \JWTAuth::manager()->getJWTProvider()->decode($new_token);
+                JWTAuth::invalidate($currentToken);
+                $new_payload = JWTAuth::manager()->getJWTProvider()->decode($new_token);
                 DB::table('users')->where('id', $currentuser->id)->update(['exp' => $new_payload['exp']]);
                 return $next($request)->header("Authorization", "Bearer " . $new_token);
             }
