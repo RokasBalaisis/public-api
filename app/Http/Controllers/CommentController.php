d0e76acda2eb09e00e13e5e762cafd7f3b4da19a
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-10-29 20:53:22 +0200

    api debug

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -20,7 +20,7 @@ class CommentController extends Controller
      */
     public function index()
     {
-        dd(Auth::user()->with('role'));
+        dd(User::with('role')->find(Auth::id()));
         $comments = Comment::all();
         return response()->json(['comments' => $comments], 200);
     }
