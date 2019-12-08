f6edd9fd50b8d5a9d4c1272c997c76f9266342db
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-12-08 21:47:26 +0200

    api update

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -59,7 +59,7 @@ class MediaController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'category_id' => ['exists:categories,id', 'required', 'integer'],
-            'name' => ['required', 'alphanum'],
+            'name' => ['required', 'alpha_dash'],
             'short_description' => ['required'],
             'description' => ['required'],
             'trailer_url' => ['required', 'regex:/www.youtube(?:-nocookie)?.com\/(?:v|embed)\/([a-zA-Z0-9-_]+).*/'],
