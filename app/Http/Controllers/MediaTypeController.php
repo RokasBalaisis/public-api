c73cd243720308516f371c16e2f6f354c8e4913c
RokasBalaisis <rokas.balaisis@ktu.edu>
2020-01-31 01:54:22 +0200

    debug

--- app/Http/Controllers/MediaTypeController.php
+++ app/Http/Controllers/MediaTypeController.php
@@ -73,8 +73,8 @@ class MediaTypeController extends Controller
         $allMediaTypes = MediaType::all();
         foreach($allMediaTypes as $mediaType)
         {
-            $mediaArray = MediaType::with('media.ratings', 'media.cover')->where('name', $mediaType->name)->get()->media->sortByDesc('created_at')->take(12);
-            dd($mediaArray);
+            $mediaArray = MediaType::with('media.ratings', 'media.cover')->where('name', $mediaType->name)->get();  //->media->sortByDesc('created_at')->take(12);
+            dd($mediaArray->keys());
             foreach($mediaArray as $entry)
             {
                 array_push($media_types, $entry);
