09fbfa6a72f0e457d9de27263a34a6619c6b9a34
RokasBalaisis <rokas.balaisis@ktu.edu>
2020-02-16 19:33:55 +0200

    fixed media by name function

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -341,7 +341,7 @@ class MediaController extends Controller
     public function getMediaByName($media_file_name)
     {
         $parsedName = str_replace("_", " ", $media_file_name);
-        if(Media::find($parsedName) === null)
+        if(Media::with('files', 'actors')->where('name', $parsedName)->first() === null)
             return response()->json(['message' => 'Media with specified name does not exist'], 404);
         $media = Media::with('files', 'actors')->where('name', $parsedName)->first();
         $media->files->transform(function ($item) {
